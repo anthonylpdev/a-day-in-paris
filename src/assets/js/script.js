746510df8fc6d9073375c85e49a78d064ab5bd91
Anthony Lopez <alopez@inbenta.com>
2020-11-24 14:38:10 +0100

    Ft transition - Improve transition animations

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -19,19 +19,18 @@ class Base {
     this.rect = this.ref.getBoundingClientRect();
     this.elList = gsap.utils.toArray('.parallax-el');
     this.isMoving = false;
+    this.variation = document.querySelector('#variation');
 
     // Parcel hot module replacement (HMR)
     if (module.hot) {
       module.hot.accept();
     }
 
+    this.titles = document.querySelectorAll('.moment-title');
     this.splited = [];
-    document.querySelectorAll('.moment-title').forEach((el) => {
+    this.titles.forEach((el) => {
       this.splited.push(new SplitText(el, {type:"chars"}));
     });
-
-    console.log(this.splited);
-
   }
   listener() {
     if (!this.isMobile) {
@@ -40,21 +39,59 @@ class Base {
       }, false);
     }
 
+    let mainSelector = document.querySelector('main');
+
     document.querySelector('#switch').addEventListener('click', (event)  => {
       event.preventDefault();
+      event.target.disabled = true;
       let nextScene = this.currentScene >= this.splited.length - 1 ? 0 : this.currentScene + 1;
-      console.log(this.splited[nextScene]);
+
+      //let newClass = this.titles[this.currentScene].getAttribute('data-slug');
+      console.log(this.variation.children[this.currentScene].children, this.variation.children[nextScene].children);
       gsap.timeline()
+      .addLabel('begin')
       .to(this.splited[this.currentScene].chars, {
         opacity: 0,
-        stagger: 0.1
-      })
-      .to(this.splited[nextScene].chars, {
+        y: -40,
+        stagger: 0.05,
+        ease: "expo.in"
+      }, 'begin')
+      .to(this.variation.children[this.currentScene].children, {
+        opacity: 0,
+        y: -40,
+        stagger: 0.05,
+        ease: "expo.in",
+        onComplete: () => {
+          mainSelector.classList = document.querySelectorAll('.moment-title')[nextScene].getAttribute('data-slug') + ' transition';
+        }
+      }, 'begin')
+      .fromTo(this.splited[nextScene].chars, {
+        opacity: 0,
+        y: 40,
+        stagger: 0.1,
+        ease: "expo.in"
+      },{
         opacity: 1,
-        stagger: 0.1
+        y: 0,
+        stagger: 0.1,
+        ease: "expo.out"
+      })
+      .fromTo(this.variation.children[nextScene].children, {
+        opacity: 0,
+        y: 40,
+        stagger: 0.05,
+        ease: "expo.out"
+      }, {
+        opacity: 1,
+        y: 0,
+        stagger: 0.05,
+        duration: 2,
+        ease: "expo.out",
+        onComplete: () => {
+          event.target.disabled = false;
+        }
       })
       .play();
-
       this.currentScene = nextScene;
     });
 
@@ -74,7 +111,7 @@ class Base {
         gsap.to(el, {
           x: this.rect.width * this.mouse.x * (index + 1.2) / speed,
           y: this.rect.height * this.mouse.y * (index + 1.2) / speed,
-        });
+        }).play();
       });
       this.isMoving = false;
     }
